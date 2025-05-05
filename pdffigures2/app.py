300999725bafafda7df03fd9f58dfef668742d1b
JebM <jabethm@gmail.com>
2025-05-05 12:39:37 +0100

    Seperation by filename

--- pdffigures2/app.py
+++ pdffigures2/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import FileResponse
 import os
 import subprocess
@@ -10,10 +10,13 @@ import uuid
 app = FastAPI()
 
 @app.post("/extract/")
-async def extract_figures(file: UploadFile = File(...)):
-    input_dir = "/app/input"
-    output_dir = "/app/output"
+async def extract_figures(file: UploadFile = File(...), file_name:str = Form(...)):
+    input_dir = f"/app/input/{file_name}"
+    output_dir = f"/app/output/{file_name}"
     input_path = os.path.join(input_dir, file.filename)
+    
+    clear_directory(input_dir)
+    clear_directory(output_dir)
 
     # Save uploaded file
     with open(input_path, "wb") as f:
@@ -44,7 +47,7 @@ async def extract_figures(file: UploadFile = File(...)):
 
     if not files:
         return {"error": "No output files were generated."}
-
+    
     # Create a zip archive of results
     zip_filename = f"/app/output/extracted_{uuid.uuid4().hex[:8]}.zip"
     with zipfile.ZipFile(zip_filename, "w") as zipf:
@@ -53,3 +56,17 @@ async def extract_figures(file: UploadFile = File(...)):
             zipf.write(file_path, arcname=arcname)
 
     return FileResponse(zip_filename, media_type="application/zip", filename=os.path.basename(zip_filename))
+
+def clear_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)  # Create the folder if it doesn't exist
+        return
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.remove(file_path)  # Remove file or symlink
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)  # Remove directory
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
